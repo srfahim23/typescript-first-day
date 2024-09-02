ae05a14aa65ee17da2f0fa348992a81299fb775c
smuan <smuan@localhost.localdomain>
2024-09-02 19:39:26 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// besic coding in ts ?
+var userName;
+userName = "Fahim Faysal";
+var age = 20;
+console.log(userName);
+console.log(age);
+// basic function 
+function addNumbers(num1, num2) {
+    console.log(num1 + num2);
+}
+addNumbers(20, 30);
+// addNumbers(20, "30");  // this is gonna be wrong and expected error message
---
+++ index.ts
@@ -0,0 +1,18 @@
+// besic coding in ts ?
+
+var userName :string;
+userName = "Fahim Faysal";
+
+var age :number = 20;
+console.log(userName);
+console.log(age)
+
+
+
+// basic function 
+function addNumbers(num1: number, num2:number){
+    console.log(num1+ num2)
+}
+
+addNumbers(20, 30);
+// addNumbers(20, "30");  // this is gonna be wrong and expected error message
